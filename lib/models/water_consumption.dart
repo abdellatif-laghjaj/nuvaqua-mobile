442f292aa6a30d4f7d654f32b66c4ecf21a6302e
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-08-08 16:02:47 +0100

    Create water_consumption.dart

---
+++ lib/models/water_consumption.dart
@@ -0,0 +1,13 @@
+class WaterConsumption {
+  final double currentMonthConsumption;
+  final List<double> dailyConsumption;
+  final List<double> weeklyConsumption;
+  final List<double> monthlyConsumption;
+
+  WaterConsumption({
+    required this.currentMonthConsumption,
+    required this.dailyConsumption,
+    required this.weeklyConsumption,
+    required this.monthlyConsumption,
+  });
+}
