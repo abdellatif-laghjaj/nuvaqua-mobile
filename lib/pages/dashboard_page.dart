3de6fd5d886dc707db6a42d2bb38b9c03ed5aaff
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-08-08 16:11:30 +0100

    Update dashboard_page.dart

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -28,9 +28,6 @@ class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('لوحة التحكم'),
-      ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
