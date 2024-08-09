7d76c1c5ad6a11b9453f9e8e9f39a9e9de0c5182
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-08-09 23:31:50 +0100

    Update home_owner.dart

--- lib/models/home_owner.dart
+++ lib/models/home_owner.dart
@@ -24,6 +24,62 @@ class HomeOwner {
     required this.waterMeters,
     required this.reclamations,
   });
+
+  static fromJson(Map<String, dynamic> json) {
+    return HomeOwner(
+      id: json['id'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      cin: json['cin'],
+      phone: json['phone'],
+      email: json['email'],
+      createdAt: DateTime.parse(json['createdAt']),
+      updatedAt: DateTime.parse(json['updatedAt']),
+      deletedAt: json['deletedAt'],
+      waterMeters: json['waterMeters']
+          .map<WaterMeter>((meter) => WaterMeter(
+                id: meter['id'],
+                meterNumber: meter['meterNumber'],
+                ownerId: meter['ownerId'],
+                consumptionTypeId: meter['consumptionTypeId'],
+                locationId: meter['locationId'],
+                installationDate: DateTime.parse(meter['installationDate']),
+                createdAt: DateTime.parse(meter['createdAt']),
+                updatedAt: DateTime.parse(meter['updatedAt']),
+                deletedAt: meter['deletedAt'],
+              ))
+          .toList(),
+      reclamations: json['reclamations']
+          .map<Reclamation>((reclamation) => Reclamation(
+                id: reclamation['id'],
+                ownerId: reclamation['ownerId'],
+                subject: reclamation['subject'],
+                message: reclamation['message'],
+                status: reclamation['status'],
+                createdAt: DateTime.parse(reclamation['createdAt']),
+                updatedAt: DateTime.parse(reclamation['updatedAt']),
+                deletedAt: reclamation['deletedAt'],
+              ))
+          .toList(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'firstName': firstName,
+      'lastName': lastName,
+      'cin': cin,
+      'phone': phone,
+      'email': email,
+      'createdAt': createdAt.toIso8601String(),
+      'updatedAt': updatedAt.toIso8601String(),
+      'deletedAt': deletedAt,
+      'waterMeters': waterMeters.map((meter) => meter.toJson()).toList(),
+      'reclamations':
+          reclamations.map((reclamation) => reclamation.toJson()).toList(),
+    };
+  }
 }
 
 class Reclamation {
@@ -46,6 +102,32 @@ class Reclamation {
     required this.updatedAt,
     required this.deletedAt,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'ownerId': ownerId,
+      'subject': subject,
+      'message': message,
+      'status': status,
+      'createdAt': createdAt.toIso8601String(),
+      'updatedAt': updatedAt.toIso8601String(),
+      'deletedAt': deletedAt,
+    };
+  }
+
+  static fromJson(Map<String, dynamic> json) {
+    return Reclamation(
+      id: json['id'],
+      ownerId: json['ownerId'],
+      subject: json['subject'],
+      message: json['message'],
+      status: json['status'],
+      createdAt: DateTime.parse(json['createdAt']),
+      updatedAt: DateTime.parse(json['updatedAt']),
+      deletedAt: json['deletedAt'],
+    );
+  }
 }
 
 class WaterMeter {
@@ -70,4 +152,32 @@ class WaterMeter {
     required this.updatedAt,
     required this.deletedAt,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'meterNumber': meterNumber,
+      'ownerId': ownerId,
+      'consumptionTypeId': consumptionTypeId,
+      'locationId': locationId,
+      'installationDate': installationDate.toIso8601String(),
+      'createdAt': createdAt.toIso8601String(),
+      'updatedAt': updatedAt.toIso8601String(),
+      'deletedAt': deletedAt,
+    };
+  }
+
+  static fromJson(Map<String, dynamic> json) {
+    return WaterMeter(
+      id: json['id'],
+      meterNumber: json['meterNumber'],
+      ownerId: json['ownerId'],
+      consumptionTypeId: json['consumptionTypeId'],
+      locationId: json['locationId'],
+      installationDate: DateTime.parse(json['installationDate']),
+      createdAt: DateTime.parse(json['createdAt']),
+      updatedAt: DateTime.parse(json['updatedAt']),
+      deletedAt: json['deletedAt'],
+    );
+  }
 }
